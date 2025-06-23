00c5c8e73756578716f8e50233180d79c7dde624
yanbo <yanbophx@hotmail.com>
2025-06-23 10:11:52 +0800

    chore: save latest bottle, table, and loader before merging to master

--- loadRobotBottleTable.m
+++ loadRobotBottleTable.m
@@ -12,4 +12,38 @@ eePosBs = sampleReachableWorkspace(robotB, endEffectorNameB);
 save ('bottle.mat','bottleInB');
 save ('table.mat','tableBox');
 
+% Check if bottle's grasp pose is reachable
+graspPos = tform2trvec(bottleInB.graspPose);
+dists = vecnorm(eePosBs - graspPos, 2, 2);
+if all(dists > 0.05)
+    warning('Bottle grasp pose is NOT within 5 cm of any sampled reachable workspace point!');
+    % Move bottle to the closest reachable point
+    [~, idxClosest] = min(dists);
+    newBottlePos = eePosBs(idxClosest, :);
+    fprintf('Trying to move bottle to closest reachable point: (%.3f, %.3f, %.3f)\n', newBottlePos(1), newBottlePos(2), newBottlePos(3));
+    % Update bottle pose and grasp pose
+    bottleCandidate = bottleInB;
+    bottleCandidate.pose = trvec2tform(newBottlePos);
+    T_graspLocal = bottleCandidate.pose \ bottleCandidate.graspPose;
+    bottleCandidate.graspPose = bottleCandidate.pose * T_graspLocal;
+    % Try IK for the new grasp pose
+    gik = generalizedInverseKinematics('RigidBodyTree', robotB, 'ConstraintInputs', {'pose','joint'});
+    poseTgt = constraintPoseTarget(endEffectorNameB);
+    poseTgt.TargetTransform = bottleCandidate.graspPose;
+    poseTgt.Weights = [1,1];
+    jointTgt = constraintJointBounds(robotB);
+    initialGuess = homeConfiguration(robotB);
+    [qSol, solutionInfo] = gik(initialGuess, poseTgt, jointTgt);
+    if strcmpi(solutionInfo.Status, 'success')
+        disp('IK succeeded for new bottle position. Bottle updated.');
+        bottleInB = bottleCandidate;
+        save('bottle.mat','bottleInB');
+        save('table.mat','tableBox');
+    else
+        warning('IK failed for the closest reachable point. Bottle not updated.');
+    end
+else
+    disp('Bottle grasp pose is reachable by the arm.');
+end
+
 % this file is only for saving bottle and table as object 
