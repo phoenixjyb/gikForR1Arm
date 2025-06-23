5b4cca135cbcd301f0385035b2dc7d67e2b6ed03
yanbo <yanbophx@hotmail.com>
2025-06-23 19:18:33 +0800

    Remove backupFiles folder from master branch for a clean project structure (now exclusive to backupFiles-only branch)

--- backupFiles/publishSourceTrajectory.m
+++
@@ -1,6 +0,0 @@
-function publishSourceTrajectory(node, topicName, T_all)
-    pub = ros2publisher(node, topicName, 'geometry_msgs/PoseArray');
-    msg = transformTrajectoryToPoseArrayMsg(T_all, 'torso_link');
-    send(pub, msg);
-    disp("Published EE trajectory with " + size(T_all,3) + " steps.");
-end
--- backupFiles/vectorToConfig.m
+++
@@ -1,7 +0,0 @@
-function cfgStruct = vectorToConfig(robot, qVec)
-    cfgTemplate = homeConfiguration(robot);
-    for i = 1:numel(cfgTemplate)
-        cfgTemplate(i).JointPosition = qVec(i);
-    end
-    cfgStruct = cfgTemplate;
-end
